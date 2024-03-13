6733fad75ad485ff1ae2c25de6ef85cc27e41878
Gonzalo Varela <88951520+LaPiragna@users.noreply.github.com>
2024-03-13 13:37:52 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-void modifyString(char* str, char replacement[50]);
-
 int main() {
-    char str[50], str1[50];
-    int numero = 30;
-    boolean flag = false;
-    printf("Enter a string: ");
-    scanf("%s", &str1);
-    modifyString(str, str1);
-    printf("Modified string: %s\n", str);
-
+    printf("Hello World!");
     return 0;
-}
-
-void modifyString(char* str, char replacement[50]) {
-    strcpy(str, replacement);
 }

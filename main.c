8bb4f34b0263fade571ff3b9130e3b67cf17584c
unknown <gonzalovarelaalagna@gmail.com>
2024-03-14 12:26:38 -0300

    This is C bruh

--- main.c
+++ main.c
@@ -3,14 +3,13 @@
 #include <time.h>
 
 int main() {
-    System.out.println("Hello World!");
-    System.out.println("I am learning!");
+    printf("Hello World!\n");
+    printf("I am learning!\n");
 
-    /*Message below actually was a merge in 'master' branch with 'test' branch.*/
-    System.out.println("Changes on test branch* would be the correct description of this commit bro!~ bye!");
+    /* Message below actually was a merge in 'master' branch with 'test' branch. */
+    printf("Changes on test branch* would be the correct description of this commit bro!~ bye!\n");
 
-
-    System.out.println("Commit made from git!");
+    printf("Commit made from git!\n");
 
     return 0;
 }

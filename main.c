a83672767ce2867ecc95c6e0d80db594220b85d4
unknown <gonzalovarelaalagna@gmail.com>
2024-03-14 12:19:42 -0300

    New function

--- main.c
+++ main.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
 #include <string.h>
 
+void message(char *str, char *str1);
+
 int main() {
-    System.out.println("Hello World!");
-    System.out.println("I am learning!");
+    printf("Hello World!\n");
+    printf("I am learning!\n");
 
-    /*Message below actually was a merge in 'master' branch with 'test' branch.*/
-    System.out.println("Changes on test branch* would be the correct description of this commit bro!~ bye!");
+    /* Message below actually was a merge in 'master' branch with 'test' branch. */
+    printf("Changes on test branch* would be the correct description of this commit bro!~ bye!\n");
 
+    printf("Commit made from git!\n");
 
-    System.out.println("Commit made from git!");
-    
+    char str[100]; // Allocate memory for the strings
+    char str1[100];
+
+    printf("Enter a message: ");
+    // fflush(stdin); // fflush on stdin is undefined behavior in C, should be avoided
+    scanf("%99s", str1); // Limit input to prevent buffer overflow
+    message(str, str1);
+    printf("New message: %s.\n", str);
+
     return 0;
+}
+
+void message(char *str, char *str1) {
+    strcpy(str, str1); // Copy the content of str1 to str
 }

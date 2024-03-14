b6396c7c80c6538e95d5b5b141bf5351448f933f
unknown <gonzalovarelaalagna@gmail.com>
2024-03-14 10:49:02 -0300

    messy

--- main.c
+++ main.c
@@ -4,6 +4,6 @@
 int main() {
     System.out.println("Hello World!");
     System.out.println("I am learning!");
-    System.out.println("Changes on test branch* would be the correct description of this commit bro!~ bye!")
+    System.out.println("Changes on test branch* would be the correct description of this commit bro!~ bye!");
     return 0;
 }
